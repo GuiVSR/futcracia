ed646609f0479bc88c3b381b73ac74b099cce5c2
GuiVSR <guivribeiro@gmail.com>
2025-01-28 17:49:52 -0300

    fix tests

--- test/controllers/poll_ax_bs_controller_test.rb
+++ test/controllers/poll_ax_bs_controller_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class PollAxBsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @poll_ax_b = poll_ax_bs(:one)
+  end
+
   test "should get vote_for_a" do
-    get poll_ax_bs_vote_for_a_url
+    post vote_for_a_poll_ax_b_url(@poll_ax_b)
     assert_response :success
   end
 
   test "should get vote_for_b" do
-    get poll_ax_bs_vote_for_b_url
+    post vote_for_b_poll_ax_b_url(@poll_ax_b)
     assert_response :success
   end
 end
